e3f8e8e55a51e5508692398781f1f1656c15f348
Rajesh Kumar <rajesh.kumar@hbgusa.com>
2022-03-29 12:01:53 +0530

    address-book-setup

---
+++ src/main/java/com/address/book/addressbookapi/AddressBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.address.book.addressbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/address/book/addressbookapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.address.book.addressbookapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AddressBookApiApplication.class);
+	}
+
+}
